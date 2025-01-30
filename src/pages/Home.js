d9ef313b26c5ef80c737d6553024bae04ec34fae
Wanderson notebook <wandersonlife7@gmail.com>
2025-01-30 11:07:15 -0300

    initializing navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        Olá<span>👋</span> sou Wanderson
+      </p>
+      <h3>Site em desenvolvimento...</h3>
+    </div>
+  );
+};
+
+export default Home;
